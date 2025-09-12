3b7de4bb73d4be5361d837f2a2c3915068630d01
random393939 <jude.donelson@gmail.com>
2025-09-12 10:04:20 -0700

    Set up github and added player amount in Run.java

--- Run.java
+++ Run.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class Run {
     public static void main(String[] args) {
         Deck deck = new Deck();
-
+        
         for(Card card : deck.getCards())
         {
             System.out.println(card);
         }
+
+        //Player amount
+        Scanner playerAmount = new Scanner(System.in); 
+        System.out.println("Enter number of players");
+
+        String players = playerAmount.nextLine();
+        System.out.println(players +" players");
+
     }
 }
