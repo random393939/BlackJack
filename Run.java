98c7e46439c7a3acc526b8fb4c863329b2d31614
random393939 <jude.donelson@gmail.com>
2025-09-19 09:57:55 -0700

    added player names and their names into an Arraylist

--- Run.java
+++ Run.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Run {
     public static void main(String[] args) {
         Deck deck = new Deck();
@@ -10,6 +11,7 @@ public class Run {
 
         //Player amount
         boolean player_step = true;
+        ArrayList<String> player_List = new ArrayList<String>();
         while(player_step){
             Scanner integScanner = new Scanner(System.in); 
             System.out.println("Enter number of players (up to 4)");
@@ -19,7 +21,22 @@ public class Run {
 
             if(players < 5 && players > 0) {
                 System.out.println(players +" players selected");
+                for(int j = 1;j<(players+1);j++){
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.println("What is your player name, player"+j);
+
+                    String player_name = scanner.next();
+                    player_List.add(player_name);
+                    scanner.nextLine();
+                }
+                System.out.println(player_List);
                 player_step = false;
+                boolean player_bet_amount = true;
+                while(player_bet_amount){
+                    for(int x = 0; x > players;x++){
+                        System.out.println(player_List); //Left off here
+                    }
+                }
             }
             else{
                 System.out.println("please select up to but not over 4 players");
